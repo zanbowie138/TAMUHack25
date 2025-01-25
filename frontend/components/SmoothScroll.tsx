db3d31d70a235729171766b8911ead860d13354b
luvadlamudi <lucasvad123@gmail.com>
2025-01-25 16:57:56 -0600

    landing page

---
+++ frontend/components/SmoothScroll.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Lenis from "lenis";
+import { useEffect, useRef } from "react";
+
+interface SmoothScrollProps {
+  children: React.ReactNode;
+}
+
+export default function SmoothScroll({ children }: SmoothScrollProps) {
+  const lenisRef = useRef<Lenis | null>(null);
+
+  useEffect(() => {
+    lenisRef.current = new Lenis({
+      duration: 1.2,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: number) {
+      if (lenisRef.current) {
+        lenisRef.current.raf(time);
+        requestAnimationFrame(raf);
+      }
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      if (lenisRef.current) {
+        lenisRef.current.destroy();
+      }
+    };
+  }, []);
+
+  return <>{children}</>;
+}
