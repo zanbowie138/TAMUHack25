929528fcba0aca61729321c0e15d31e5479c173e
Jadon Lee <jadonlee888@gmail.com>
2025-01-26 00:45:49 -0600

    lazy table create and dont use this pickle

--- backend/table_create.sql
+++ backend/table_create.sql
@@ -19,12 +19,12 @@ CREATE TABLE cars
     car_model VARCHAR(100) NOT NULL, -- Name of the car (e.g., "Corolla")
     car_year INT NOT NULL,          -- Year of the car (e.g., 2020)
     car_name VARCHAR(150) GENERATED ALWAYS AS (car_model || ' ' || car_year) STORED, -- I am the SQL mastaaahh
-    msrp INT,                      -- Manufacturer's Suggested Retail Price
+    msrp VARCHAR(150),                      -- Manufacturer's Suggested Retail Price. Varchar because im too lazy to edit the pickle
     horsepower INT,               -- Horsepower of the car
     mpg INT,                   -- Miles per gallon
-    num_seats INT,               -- Number of seats in the car
+    num_seats VARCHAR(150),               -- Number of seats in the car. Varchar for same reason as msrp
     drive_type VARCHAR(100), -- Type of drive (e.g., "Front Wheel Drive", "Rear Wheel Drive")
-	UNIQUE (car_name),      -- Ensure car is unique
+	UNIQUE (car_name)      -- Ensure car is unique
 );
 
 CREATE TABLE car_reviews
