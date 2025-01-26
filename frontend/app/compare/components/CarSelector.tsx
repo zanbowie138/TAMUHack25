dd4dd5aa95d7c1736c072136888afed7b27591d1
Jadon Lee <jadonlee888@gmail.com>
2025-01-25 18:06:10 -0600

    updates to selectors for compare

--- frontend/app/compare/components/CarSelector.tsx
+++ frontend/app/compare/components/CarSelector.tsx
@@ -1,19 +1,26 @@
 import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react'
+import { X } from 'lucide-react';
+import Image from 'next/image';
 import { useState } from 'react'
 
 // Define the Car type
 interface Car {
   id: number;
+  model: string;
+  year: string;
   name: string;
 }
 
-const cars: Car[] = [
-  { id: 1, name: 'Camry' },
-  { id: 2, name: 'Tundra' },
-  { id: 3, name: 'Rav4' },
-  { id: 4, name: 'Corolla' },
-  { id: 5, name: 'Prius' },
-]
+const cars: Car[] = []
+
+const models = ['prius', 'camry', 'corolla', 'highlander', 'rav4', 'sienna', 'tacoma', 'tundra']
+const years = ['2025', '2024', '2023', '2022', '2021', '2020']
+
+for (const model of models) {
+  for (const year of years) {
+    cars.push({ id: cars.length, model: `${model}`, year: `${year}`, name: `${year} ${model}` })
+  }
+}
 
 export default function CarSelector() {
   const [selectedCar, setSelectedCar] = useState(cars[0])
@@ -27,18 +34,27 @@ export default function CarSelector() {
       })
 
   return (
-    <div className="w-80 h-96 bg-zinc-800 rounded-xl border-gray-500 border-2 p-4 flex flex-col">
+    <div className="w-80 h-96 bg-zinc-800 rounded-xl border-gray-500 border-2 p-4 flex flex-col relative">
+      <div className='absolute top-0 right-0'>
+        <X color='#ffffff' strokeWidth={1} className='w-6 h-auto'/>
+      </div>
+
+      <Image src={`https://www.toyota.com/imgix/content/dam/toyota/jellies/relative/${selectedCar.year}/${selectedCar.model}/base.png`} alt="Car Image" width={1000} height={1000}/>
+      <div className='w-full text-center text-white'>[review stars]</div>
+      <div className='w-full grow border-2 border-white rounded-md my-1.5'>
+        [ai summary and sparkle]
+      </div>
       <div className='mt-auto'>
-        <Combobox value={selectedCar} onChange={(value) => setSelectedCar(value ?? cars[0])} onClose={() => setQuery('')}>
+        <Combobox immediate value={selectedCar} onChange={(value) => setSelectedCar(value ?? cars[0])} onClose={() => setQuery('')}>
           <ComboboxInput
             aria-label="Assignee"
             displayValue={(car: Car) => car?.name}
             onChange={(event) => setQuery(event.target.value)}
             className={`w-full h-14 border-2 border-slate-200 rounded-md text-xl text-white p-2`}
           />
-          <ComboboxOptions anchor="bottom" className="border empty:invisible w-72">
+          <ComboboxOptions anchor="bottom" className="border empty:invisible w-72 mt-1 rounded-md">
             {filteredCars.map((car) => (
-              <ComboboxOption key={car.id} value={car} className="flex gap-2 bg-white data-[focus]:bg-blue-100">
+              <ComboboxOption key={car.id} value={car} className="flex gap-2 bg-gray-800 data-[focus]:bg-gray-900 p-2 opacity-80 hover:opacity-100 text-white backdrop-blur-lg">
                 {car.name}
               </ComboboxOption>
             ))}
--- frontend/app/compare/components/SelectorGroup.tsx
+++ frontend/app/compare/components/SelectorGroup.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CarSelector from './CarSelector';
+import { CirclePlus } from 'lucide-react';
 
 export default function SelectorGroup() {
+  const[numSelectors, setNumSelectors] = useState(2)
+
   return (
     <div className='flex flex-row w-full justify-center space-x-12'>
       <CarSelector />
-      <CarSelector />
+      <CarSelector /> {/* TODO: allow it to add more selectors and remove selectors */}
+      {/* {numSelectors < 4 ? <button onClick={() => setNumSelectors(numSelectors + 1)}><CirclePlus color='#ffffff' strokeWidth={1} className='w-20 h-auto'/></button> : <></>} */}
     </div>
   )
 }
